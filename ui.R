0fec7d7abf660e2d317cb3986f91699576e8bfc8
Juan Álvarez Martínez <juan4@illinois.edu>
2022-12-11 04:32:26 -0600

    Add navigation menu to the sidebar

--- ui.R
+++ ui.R
@@ -10,32 +10,69 @@ source('functions/helpers.R')
 
 shinyUI(
     dashboardPage(
-          skin = "blue",
-          dashboardHeader(title = "Book Recommender"),
-          
-          dashboardSidebar(disable = TRUE),
-
-          dashboardBody(includeCSS("css/books.css"),
-              fluidRow(
-                  box(width = 12, title = "Step 1: Rate as many books as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
-                      div(class = "rateitems",
-                          uiOutput('ratings')
-                      )
-                  )
-                ),
-              fluidRow(
-                  useShinyjs(),
-                  box(
-                    width = 12, status = "info", solidHeader = TRUE,
-                    title = "Step 2: Discover books you might like",
-                    br(),
-                    withBusyIndicatorUI(
-                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+        skin="blue",
+        dashboardHeader(title="Movie Recommender"),
+        dashboardSidebar(
+            sidebarMenu(
+                menuItem("Recommender by Genre", icon=icon("film"), tabName="system_1"),
+                menuItem("Recommender by Rating", icon=icon("star"), tabName="system_2")
+            )
+        ),
+        dashboardBody(
+            includeCSS("css/books.css"),
+            tabItems(
+                tabItem(
+                    tabName="system_1",
+                    fluidRow(
+                        box(
+                            width=12,
+                            title="Step 1: Select your favorite genre",
+                            status="info",
+                            solidHeader=TRUE,
+                            collapsible=TRUE,
+                            div(class="rateitems"),
+                        )
                     ),
-                    br(),
-                    tableOutput("results")
-                  )
-               )
-          )
+                    fluidRow(
+                        useShinyjs(),
+                        box(
+                            width=12,
+                            status="info",
+                            solidHeader=TRUE,
+                            title="Step 2: Discover movies you might like",
+                            br(),
+                            withBusyIndicatorUI(actionButton("btn", "Click here to get your recommendations", class="btn-warning")),
+                            br(),
+                        )
+                    )
+                ),
+                tabItem(
+                    tabName="system_2",
+                    fluidRow(
+                        box(
+                            width=12,
+                            title="Step 1: Rate the following movies",
+                            status="info",
+                            solidHeader=TRUE,
+                            collapsible=TRUE,
+                            div(class="rateitems", uiOutput('ratings'))
+                        )
+                    ),
+                    fluidRow(
+                        useShinyjs(),
+                        box(
+                            width=12,
+                            status="info",
+                            solidHeader=TRUE,
+                            title="Step 2: Discover movies you might like",
+                            br(),
+                            withBusyIndicatorUI(actionButton("btn", "Click here to get your recommendations", class="btn-warning")),
+                            br(),
+                            tableOutput("results")
+                        )
+                    )
+                )
+            )
+        )
     )
-) 
+)
